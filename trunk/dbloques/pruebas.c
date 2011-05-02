8a6e81c50a3aa2ac99377bdc28d0bb6e3e9b81ee
aerivarola <aerivarola@e858ceb4-9670-091e-5e61-07aac34ba807>
2011-05-02 15:59:02 +0000

    eliminado archivos que no se usan

--- trunk/dbloques/pruebas.c
+++ trunk/dbloques/pruebas.c
@@ -7,6 +7,39 @@
  *  Archivo para ir realizando las pruebas de las funciones
  */
 
-#include "comun.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+/*
+void main(int argc, char* argv[]){
+	int i, j = 0,
+		div = 3,
+		salto = 0;
+
+	for( i = 0; i < div*div; i++){
+
+		if ( i < div ){
+			salto = j * div;
+			printf( "salto1: %d, \n", salto);
+		}
+		else if( i % div == 0){
+			j = 0;
+			salto = j * div;
+			printf( "salto2: %d, \n", salto);
+		}else{
+			salto = j * div;
+			printf( "salto3: %d, \n", salto);
+		}
+		j++;
+
+		printf( "Posicion de columna: %d, tarea: %d \n", salto, i);
+	}
 
 
+	//j = ( i / div );
+	//j = (j -1)*div;
+
+
+}
+
+*/
